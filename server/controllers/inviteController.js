782c161dac9e556f09b1621606689396144167a9
solankiamn8 <solankiamn8@gmail.com>
2026-02-02 01:34:49 +0530

    remove extra / from link

--- server/controllers/inviteController.js
+++ server/controllers/inviteController.js
@@ -47,7 +47,7 @@ export const inviteUser = async (req, res) => {
   }
 
   const token = crypto.randomBytes(32).toString("hex");
-  const baseUrl = process.env.FRONTEND_URL || "https://fleetmanagerpro22.netlify.app/"
+  const baseUrl = process.env.FRONTEND_URL || "https://fleetmanagerpro22.netlify.app"
   const inviteLink = `${baseUrl}/accept-invite?token=${token}`;
 
   let invite; // 🔑 important
